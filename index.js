829010d958b3d8f8e1cdb9999ec965e73dd91e69
Neeraj Singh <nsrneerajsingh2721@gmail.com>
2023-03-07 15:01:04 +0530

    added

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 // const bodyparser=require('body-parser');
-const connect = require("./config/db");
 const productRouter = require("./routes/product.route");
 
+const connect = require("./config/db");
 const app = express();
-app.use(express);
-app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// app.use(bodyparser.urlencoded({extended:false}))
-app.use(bodyparser.json());
-// app.use("/product", productRouter);
+app.use(cors());
+
+mongoose.set("strictQuery", false);
 const port = process.env.PORT;
+app.use("/product", productRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello world");
@@ -25,3 +23,34 @@ app.listen(port, async() => {
   connect();
   console.log(`local host :${port}`);
 });
+
+
+// require("dotenv").config();
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const bodyparser=require('body-parser');
+
+// const connect = require("./config/db");
+// const port = process.env.PORT || 8000;
+
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+// app.use(bodyparser.urlencoded({extended:false}))
+// app.use(bodyparser.json());
+
+// mongoose.set("strictQuery", false);
+
+
+
+// app.get("/", (req, res) => {
+// 	res.send("hello world!");
+// });
+
+
+// app.listen(port, async () => {
+// 	connect()
+// 	console.log(`Listening at http://localhost:${port}`);
+// });
--- routes/product.route.js
+++ routes/product.route.js
@@ -8,27 +8,37 @@ productRouter.post("/", async (req, res) => {
   const data = req.body;
   try {
     for (let i = 0; i < data.length; i++) {
+      console.log(data[i])
       if (data[i].operation === "add") {
-        const pro = await ProductModel.findById({ id: data[i].productId });
-        if (pro) {
+        const pro = await ProductModel.find({ productId: data[i].productId });
+        console.log(pro)
+        if (pro.length > 0) {
           const product = await ProductModel.findByIdAndUpdate(
-            { id: data[i].productId },
-            { quantity: pro.quantity + data[i].quantity }
+            { _id: pro[0]._id },
+            { quantity: pro[0].quantity + data[i].quantity }
           );
+          console.log("Add",product)
         } else {
           const product = await ProductModel.create({
             productId: data[i].productId,
             quantity: 0,
           });
+          console.log(product)
         }
-      } else if (data[i].operation === "substract") {
-        const pro = await ProductModel.findById({ id: data[i].productId });
-        if (pro) {
+      } else if (data[i].operation === "subtract") {
+        const pro = await ProductModel.find({ productId: data[i].productId });
+        let diff = pro[0].quantity - data[i].quantity 
+        if (pro.length > 0 && diff >= 0) {
           const product = await ProductModel.findByIdAndUpdate(
-            { id: data[i].productId },
-            { quantity: pro.quantity - data[i].quantity }
+            { _id: pro[0]._id },
+            { quantity: diff}
           );
-        } else {
+        }else if (pro.length > 0 && diff < 0) {
+          const product = await ProductModel.findByIdAndUpdate(
+            { _id: pro[0]._id },
+            { quantity: 0}
+          );
+        }  else {
           const product = await ProductModel.create({
             productId: data[i].productId,
             quantity: 0,
@@ -36,10 +46,17 @@ productRouter.post("/", async (req, res) => {
         }
       }
     }
+    const totalpro = await ProductModel.find()
+    res.status(201).send(totalpro)
   } catch (err) {
     res.status(400).send({message:"failure",err})
   }
 });
+
+productRouter.get("/", async (req, res) => {
+  const totalpro = await ProductModel.find()
+    res.status(201).send(totalpro)
+})
 
 
 module.exports = productRouter;
